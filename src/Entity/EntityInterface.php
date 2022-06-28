96855d0cbc6811c4467597ac0012d70eb2b95595
Alexander Volle <alexander.volle@hotmail.com>
2022-06-28 22:56:09 +0200

    Add methods to EntityInterface

--- src/Entity/EntityInterface.php
+++ src/Entity/EntityInterface.php
@@ -9,4 +9,20 @@ namespace Avolle\Fotballdata\Entity;
  */
 interface EntityInterface
 {
+    /**
+     * Set a property in entity
+     *
+     * @param string $name Name of property
+     * @param mixed $value Value of property
+     * @return void
+     */
+    public function set(string $name, $value): void;
+
+    /**
+     * Get a property defined in entity
+     *
+     * @param string $name Name of property
+     * @return mixed
+     */
+    public function get(string $name);
 }
